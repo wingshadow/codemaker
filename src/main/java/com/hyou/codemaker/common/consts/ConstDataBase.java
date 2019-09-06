1d6cdf0648bfdea043093bf6216b5c0488ad8d7b
wingshadow <zhangqiyi1@163.com>
2019-09-06 11:20:58 +0800

    添加数据库类型常量

---
+++ src/main/java/com/hyou/codemaker/common/consts/ConstDataBase.java
@@ -0,0 +1,12 @@
+package com.hyou.codemaker.common.consts;
+
+/**
+ * @author fy
+ * @Title: ConstDataBase
+ * @Description: TODO
+ * @date 2019/9/6 11:18
+ */
+public class ConstDataBase {
+    public static final String DATABASE_ORACLE ="oracle";
+    public static final String DATABASE_MYSQL ="mysql";
+}
